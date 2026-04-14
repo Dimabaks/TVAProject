a8c2dfba720191247440a1277f098c76e55bc1f3
Dmitrii <dimonbacsa@gmail.com>
2026-04-14 23:14:09 +0300

    add logo

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -9,18 +9,23 @@ import SidebarNavItem from "./SidebarNavItem";
 import { useNavigate } from "react-router-dom";
 
 export default function Sidebar() {
-	const [collapsed, setCollapsed] = useState(false);
+	const [collapsed, setCollapsed] = useState(true);
 	const navigate = useNavigate();
 
 	return (
 		<div
 			className={`flex flex-col min-h-screen bg-white border-gray-300 border-r-1 transition-all duration-300 ${collapsed ? "w-15" : "min-w-50"}`}>
-			<div className="flex justify-between relative border-gray-300 border-b-1 pt-3 pb-3 pl-5 h-14">
+			<div
+				className={`flex  relative border-gray-300 border-b-1 h-14 items-center ${collapsed} ? "justify-center px-0" : "justify-between px-2 "`}>
 				<span
 					className={`font-bold transition-all duration-300 ${
-						collapsed ? "text-xs pt-2" : "text-2xl"
+						collapsed ? "" : "text-2xl"
 					}`}>
-					TVA
+					<img
+						src="../../src/img/logo.png"
+						alt="Logo"
+						className={`rounded-xl bg-gray-500 transition-all ${collapsed ? "w-10" : "w-12"}`}
+					/>
 				</span>
 				<button
 					className="absolute right-0 cursor-pointer top-4"
